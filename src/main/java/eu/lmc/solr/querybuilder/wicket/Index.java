cddf0b3ed233089c10622d55354b913f1e072348
Alexandra Vecherskaya <alexandra.vecherskaya@gmail.com>
2015-05-17 21:24:54 +0200

    Customizing Wicket

--- src/main/java/eu/lmc/solr/querybuilder/wicket/Index.java
+++ src/main/java/eu/lmc/solr/querybuilder/wicket/Index.java
@@ -1,7 +1,30 @@
 package eu.lmc.solr.querybuilder.wicket;
 
+import eu.lmc.solr.querybuilder.component.Component;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+
 /**
  * Created by avecherskaya on 17/05/15.
  */
 public class Index extends SolrWebPage {
+
+    public Index(){
+        add(new ListView<Component>("componentCache", getComponentCache().getCache()) {
+            @Override
+            protected void populateItem(ListItem<Component> listItem) {
+                listItem.add(new Label("componentCacheItems", listItem.getModel()));
+            }
+        });
+
+        /*
+        add(new ListView<Container>("userQuery",getUserQuery().getContainers()) {
+            @Override
+            protected void populateItem(ListView<Component> userQueryContainer) {
+                userQueryContainer.add(new ListItem<Component>("queryItem",userQueryContainer.getModel()));
+            }
+        });
+        */
+    }
 }
--- src/main/java/eu/lmc/solr/querybuilder/wicket/SolrSearchApplication.java
+++ src/main/java/eu/lmc/solr/querybuilder/wicket/SolrSearchApplication.java
@@ -35,4 +35,5 @@ public class SolrSearchApplication extends WebApplication {
         return componentCache;
     }
 
+
 }
